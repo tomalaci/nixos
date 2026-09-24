8674d9f0336e2a59ad82764ef6a934eb953a4905
Tomass Lacis <tomass.lacis@pm.me>
2026-09-24 21:39:17 +0200

    Update

--- modules/system/system.nix
+++ modules/system/system.nix
@@ -6,6 +6,10 @@
       "nix-command"
       "flakes"
     ];
+    extra-substituters = ["https://cache.numtide.com"];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
     auto-optimise-store = true;
   };
   hardware.enableRedistributableFirmware = true;
